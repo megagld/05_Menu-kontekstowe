0802bd9043778cd05309e98f9d3249100077339b
megagld <bartlomiej.mystek@gmail.com>
2023-01-17 14:53:59 +0100

    -

--- 04_Podziel pdy do druku/podziel pdfy do druku.py
+++ 04_Podziel pdy do druku/podziel pdfy do druku.py
@@ -17,6 +17,13 @@ di={842:'297',
     1684:'594',
     2382:'841',}
 
+def rou(h):
+    global di
+    dims=di.keys()
+    for i in dims:
+        if abs(h-i)<5:return i
+    return h
+
 for x in os.listdir(input_dir):
     if not x.endswith('.pdf'):
         continue
@@ -24,16 +31,17 @@ for x in os.listdir(input_dir):
     with pdfplumber.open(y) as pdf:
         page_1 = pdf.pages[0]
         height, width = page_1.height, page_1.width
+        height=rou(height)
 
         if len(pdf.pages)>1 or (height, width)==(842,595):
-            output_dir = '{}{}'.format(Path(__file__).parent,'''\\_do druku\\_inne\\''')
+            output_dir = '{}{}'.format(input_dir,'''\\_do druku\\_inne\\''')
             Path(output_dir).mkdir(parents=True, exist_ok=True)
             shutil.copyfile(y, output_dir+x)
         elif height in di.keys():
-            output_dir = '{}{}'.format(Path(__file__).parent,'''\\_do druku\\{}\\'''.format(di[height]))
+            output_dir = '{}{}'.format(input_dir,'''\\_do druku\\{}\\'''.format(di[height]))
             Path(output_dir).mkdir(parents=True, exist_ok=True)
             shutil.copyfile(y, output_dir+x)
         else:
-            output_dir = '{}{}'.format(Path(__file__).parent,'''\\_do druku\\_do sprawdzenia\\''')
+            output_dir = '{}{}'.format(input_dir,'''\\_do druku\\_do sprawdzenia\\''')
             Path(output_dir).mkdir(parents=True, exist_ok=True)
             shutil.copyfile(y, output_dir+x)
