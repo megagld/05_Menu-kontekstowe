52483f65c67d1f4cb788466ff2b71141b472e693
megagld <bartlomiej.mystek@gmail.com>
2023-02-07 10:03:29 +0100

    -

--- 09_Usun pliki tymczasowe/usun_pliki_tymczasowe.py
+++ 09_Usun pliki tymczasowe/usun_pliki_tymczasowe.py
@@ -22,13 +22,15 @@ tails=[
 input_dir = os.getcwd()
 
 ct=0
+vol=0
 
 for path, subdirs, files in os.walk(input_dir):
     for name in files:
         for tail in tails:
             if name.endswith(tail):
-                print(os.path.join(path, name))
-                os.remove(os.path.join(path, name))
                 ct+=1
+                vol+=os.path.getsize(os.path.join(path, name))
+                os.remove(os.path.join(path, name))
+print(vol)
 
-pymsgbox.alert(text='Usunięto {} plików.'.format(ct), title='{}'.format(', '.join(tails)), button='OK')
+pymsgbox.alert(text='Usunięto {} plików, {} KB.'.format(ct, round(vol*(10**-3))), title='{}'.format(', '.join(tails)), button='OK')
