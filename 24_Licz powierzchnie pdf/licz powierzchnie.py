73d5790df51fb54fa79c3c95913a560e48514488
megagld <bartlomiej.mystek@gmail.com>
2023-06-19 11:58:06 +0200

    -

--- 24_Licz powierzchnie pdf/licz powierzchnie.py
+++ 24_Licz powierzchnie pdf/licz powierzchnie.py
@@ -11,6 +11,8 @@ wsp=0.352
 file_count=0
 area_count=0
 
+rolki={}
+
 for path,_,files in os.walk(input_dir):
     for pdf_file in files:
         if pdf_file.endswith(".pdf"):
@@ -26,10 +28,16 @@ for path,_,files in os.walk(input_dir):
                     area_count+=area
                 
                     with open("{}\{}.txt".format(input_dir,'powierzchnie'), 'a', encoding="utf-8") as file:
-                        file.write('{} - {}; h={}; w={}; a={}\n'.format(file_count,pdf_file,int(height),int(width),round(area)))
+                        file.write('{} - {}; h={}mm; w={}mm; a={}m2\n'.format(file_count,pdf_file,int(height),int(width),round((area/10**6),2)))
+                    if height not in rolki:
+                        rolki[height]=width
+                    else:
+                        rolki[height]+=width
 
 with open("{}\{}.txt".format(input_dir,'powierzchnie'), 'a', encoding="utf-8") as file:
-    file.write('Sumaryczne powierzchnia: {} m2'.format(area_count))
-    file.write('Liczba rysunków: {} szt.'.format(file_count))
+    file.write('\nSumaryczne powierzchnia: {} m2\n'.format(round(area_count/10**6,1)))
+    file.write('\nLiczba rysunków: {} szt.\n'.format(file_count))
+    for i,j in sorted(rolki.items()):
+        file.write('Rolka: {} mm : {} m\n'.format(int(i),round(j/1000,1)))
 
 pymsgbox.alert(text='Pików: {} szt., Powierzchnia: {} m2'.format(file_count,round(area_count/10**6,1)), title='{}'.format('test'), button='OK')
