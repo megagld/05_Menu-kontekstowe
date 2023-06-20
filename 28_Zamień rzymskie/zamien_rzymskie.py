8059183c1a1b578f7a693e4d5cc140916314d64d
megagld <bartlomiej.mystek@gmail.com>
2023-06-20 13:40:51 +0200

    -

---
+++ 28_Zamień rzymskie/zamien_rzymskie.py
@@ -0,0 +1,21 @@
+#bmystek
+import os
+import regex as re
+import pymsgbox
+
+input_dir = os.getcwd()
+
+reg_pat='''.+(segment|Segment)( |_)([VIXvix, ]*).+(pdf|dwg)'''
+ct=0
+
+for x in os.listdir(input_dir):
+    x_match=re.match(reg_pat,x)
+    if x_match:
+        new_name = x.replace(x_match.group(3),x_match.group(3).upper())
+        old_name = '{}\\{}'.format(input_dir,x)
+        new_name = '{}\\{}'.format(input_dir,new_name)
+        if new_name not in input_dir:
+            os.rename(old_name, new_name)
+            ct+=1
+
+pymsgbox.alert(text='Zmieniono nazwy {} plików'.format(ct))
