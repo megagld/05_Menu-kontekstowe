513ad4ae5fbb1d75b4370696177279fa0f6e1216
megagldnb <Vertesschrederkandelaber@gmail.com>
2023-06-20 18:00:42 +0200

    -

--- 05_Kopiuj strukture katalogow/kopiuj_strukture_katalogow.py
+++ 05_Kopiuj strukture katalogow/kopiuj_strukture_katalogow.py
@@ -1,19 +1,10 @@
 #bmystek
-# importing the shutil module
 import shutil
-from pathlib import Path
- 
-# importing the os module
 import os
  
-# defining the function to ignore the files
-# if present in any folder
 def ignore_files(dir, files):
     return [f for f in files if os.path.isfile(os.path.join(dir, f))]
  
-# calling the shutil.copytree() method and
-# passing the src,dst,and ignore parameter
-
 input_dir = os.getcwd()
 output_dir = '{}\\{}'.format(input_dir,'_kopia struktury katalogów')
 
